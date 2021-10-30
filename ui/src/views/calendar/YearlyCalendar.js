93f6495a14b88f31a1116b8b9add62c7ccabd0a3
DementiaMomento <jadensantoso6@gmail.com>
2021-10-31 00:05:01 +0700

    added yearly calendar, both the notepage and calendar are still untouched.

--- ui/src/views/calendar/YearlyCalendar.js
+++ ui/src/views/calendar/YearlyCalendar.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Calendar from 'react-awesome-calendar';
+
+const events = [{
+    id: 1,
+    color: '#fd3153',
+    from: '2021-05-02T18:00:00+00:00',
+    to: '2021-05-05T19:00:00+00:00',
+    title: 'Watterson orders Fried Chicken'
+}, {
+    id: 2,
+    color: '#1ccb9e',
+    from: '2021-05-01T13:00:00+00:00',
+    to: '2021-05-05T14:00:00+00:00',
+    title: 'KFsus'
+}, {
+    id: 3,
+    color: '#3694DF',
+    from: '2021-05-05T13:00:00+00:00',
+    to: '2021-05-05T20:00:00+00:00',
+    title: 'Dorime, Interimo Adapare Dorime, Ameno, Ameno, Latire'
+}];
 
 const YearlyCalendar = () => {
 
   return (
-    <>
+    <><Calendar
+    events={events}
+/>
     </>
   )
 }
